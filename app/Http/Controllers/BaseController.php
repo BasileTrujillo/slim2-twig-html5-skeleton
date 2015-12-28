15f53b683a58f21e9dfad025be57235a24d1eaea
Basile Trujillo <basile.trujillo@seritech.fr>
2015-12-28 22:51:31 +0100

    Add flash bag support

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -54,9 +54,28 @@ class BaseController
      * @param $tpl
      * @param $data
      */
-    protected function render($tpl, $data)
+    protected function render($tpl, $data = array())
     {
         $datas = $data + $this->defaultData;
-        $this->app->render($tpl, $datas);
+        $this->app->render($tpl, $datas + array(
+                'flashbag'  => array(
+                    'success'   => $this->session->get('success'),
+                    'info'      => $this->session->get('info'),
+                    'warning'   => $this->session->get('warning'),
+                    'danger'    => $this->session->get('error')
+                )
+            ));
+    }
+
+    /**
+     * Add a flash bag (temporary alert msg)
+     * Key values : success, info, warning, error
+     *
+     * @param $key
+     * @param $msg
+     */
+    protected function addFlashBag($key, $msg)
+    {
+        $this->session->flash($key, array_merge((array)$this->session->get($key), array($msg)));
     }
 } 
